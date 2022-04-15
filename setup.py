97e5cb3ed6d952a498a70230bf23f543faf475f0
Aleksandrina Kovachka <akovachka@expert.ai>
2022-04-15 11:53:14 +0300

    Change version to 0.1.1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@
 from setuptools import setup, find_namespace_packages  # noqa: H301
 
 NAME = "expertai-extract"
-VERSION = "0.1.0"
+VERSION = "0.1.1"
 # To install the library, run the following
 #
 # python setup.py install
